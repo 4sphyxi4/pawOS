f0fc0163af3958dedb927a7902b72e09012fec06
Patrycja Fafara <patrycja.fafara@pm.me>
2026-04-23 20:44:28 +0200

    Added submenu for the start menu to accommodate more features in tjhe future.

--- src/components/desktop/StartMenu.jsx
+++ src/components/desktop/StartMenu.jsx
@@ -1,6 +1,20 @@
 import { createPortal } from "react-dom";
+import { useState } from "react";
 
-function StartMenu({ position, menuRef, openAnimalDatabase }) {
+function StartMenu({
+  position,
+  menuRef,
+  openDashboard,
+  openAnimalDatabase,
+  openRegisterAnimal,
+  openOrganizer,
+  openMoodTracker,
+  openPawMail,
+  openMilestones,
+  openOtherProjects,
+}) {
+  const [activeSubmenu, setActiveSubmenu] = useState(null);
+
   return createPortal(
     <div
       ref={menuRef}
@@ -12,12 +26,71 @@ function StartMenu({ position, menuRef, openAnimalDatabase }) {
     >
       <ul className="start-menu-list">
         <li className="start-menu-item">
-          <button onClick={openAnimalDatabase}>Animal Database</button>
+          <button className="start-menu-button" onClick={openDashboard}>
+            Dashboard
+          </button>
         </li>
-        <li className="start-menu-item">Register Animal</li>
-        <li className="start-menu-item">Organizer</li>
-        <li className="start-menu-item">Other Projects</li>
-        <li className="start-menu-item">Other Projects</li>
+
+        <li
+          className="start-menu-item start-menu-item--has-submenu"
+          onMouseEnter={() => setActiveSubmenu("database")}
+          onMouseLeave={() => setActiveSubmenu(null)}
+        >
+          <button
+            className="start-menu-button start-menu-button--submenu"
+            onClick={openAnimalDatabase}
+          >
+            <span>Database</span>
+            <span className="submenu-arrow">▶</span>
+          </button>
+
+          {activeSubmenu === "database" && (
+            <div className="start-submenu ui-box">
+              <button
+                className="start-menu-button"
+                onClick={openAnimalDatabase}
+              >
+                View Database
+              </button>
+              <button
+                className="start-menu-button"
+                onClick={openRegisterAnimal}
+              >
+                Register Animal
+              </button>
+            </div>
+          )}
+        </li>
+
+        <li className="start-menu-item">
+          <button className="start-menu-button" onClick={openOrganizer}>
+            Organizer
+          </button>
+        </li>
+
+        <li className="start-menu-item">
+          <button className="start-menu-button" onClick={openMoodTracker}>
+            Mood Tracker
+          </button>
+        </li>
+
+        <li className="start-menu-item">
+          <button className="start-menu-button" onClick={openPawMail}>
+            PawMail
+          </button>
+        </li>
+
+        <li className="start-menu-item">
+          <button className="start-menu-button" onClick={openMilestones}>
+            Milestones
+          </button>
+        </li>
+
+        <li className="start-menu-item">
+          <button className="start-menu-button" onClick={openOtherProjects}>
+            Other Projects
+          </button>
+        </li>
       </ul>
     </div>,
     document.body,
